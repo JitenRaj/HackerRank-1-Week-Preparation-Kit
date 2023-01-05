b7958635f950dabc25786fe5b8295f22134af573
JitenRaj <ji10raj@gmail.com>
2023-01-05 21:16:09 +0530

    Day 1 Problem 2 : Mini-Max Sum : Solution

---
+++ code/src/Day01/Problem02/Solution.java
@@ -0,0 +1,48 @@
+package Day01.Problem02;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr);
+
+        long minSum = 0, maxSum = 0;
+
+        for(int i=0; i < 4; i++){
+            minSum += arr.get(i);
+        }
+
+        for(int i=1; i < 5; i++){
+            maxSum += arr.get(i);
+        }
+
+        System.out.println(minSum + " " + maxSum);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
+
